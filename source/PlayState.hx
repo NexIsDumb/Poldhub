553056c3b51e74d75fb45a686cb942df5a1ce7c7
Nile <102095746+EnderNile8182@users.noreply.github.com>
2023-07-20 19:55:50 +0200

    1

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -85,16 +85,16 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
+		['Cacata', 0.2], //From 0% to 19%
+		['Smerda', 0.4], //From 20% to 39%
+		['Male', 0.5], //From 40% to 49%
 		['Bruh', 0.6], //From 50% to 59%
-		['Meh', 0.69], //From 60% to 68%
-		['Nice', 0.7], //69%
-		['Good', 0.8], //From 70% to 79%
-		['Great', 0.9], //From 80% to 89%
-		['Sick!', 1], //From 90% to 99%
-		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
+		['Cazzo Minchia', 0.69], //From 60% to 68%
+		['Godo', 0.7], //69%
+		['Godopoli', 0.8], //From 70% to 79%
+		['Megagodo', 0.9], //From 80% to 89%
+		['ULTRA GODO!', 1], //From 90% to 99%
+		['PERFETTO!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 
 	//event variables
@@ -2335,9 +2335,9 @@ class PlayState extends MusicBeatState
 
 	public function updateScore(miss:Bool = false)
 	{
-		scoreTxt.text = 'Score: ' + songScore
-		+ ' | Misses: ' + songMisses
-		+ ' | Rating: ' + ratingName
+		scoreTxt.text = 'Punteggio: ' + songScore
+		+ ' | Mancate: ' + songMisses
+		+ ' | Valutazione: ' + ratingName
 		+ (ratingName != '?' ? ' (${Highscore.floorDecimal(ratingPercent * 100, 2)}%) - $ratingFC' : '');
 
 		if(ClientPrefs.scoreZoom && !miss && !cpuControlled)
