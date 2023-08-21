8f91b03e7360251ab0b7840e662bfef243c93720
⍚~Nex <87421482+NexIsDumb@users.noreply.github.com>
2023-08-21 13:16:09 +0200

    Cacata Finale

--- source/JenpactCutScene.hx
+++ source/JenpactCutScene.hx
@@ -4,12 +4,16 @@ import flixel.util.FlxTimer;
 import flixel.util.FlxColor;
 import flixel.FlxCamera;
 import flixel.text.FlxText;
+import flixel.tweens.FlxTween;
 import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.FlxG;
 
+using StringTools;
+
 class JenpactCutScene extends MusicBeatState
 {
+    private var lol:FlxTween;
     private var pulsantone:FlxSprite;
     private var camFollow:FlxObject;
 
@@ -38,16 +42,29 @@ class JenpactCutScene extends MusicBeatState
         camOther.bgColor.alpha = 0;
         CustomFadeTransition.nextCamera = camOther;
 
+        var shit:String = '';
+        var fuck:Array<Array<String>> = [[], []];
+        for (i => comm in ['ui_left', 'ui_down', 'ui_up', 'ui_right']) {
+            var keys:Array<Dynamic> = ClientPrefs.keyBinds.get(comm);
+            fuck[0][i] = InputFormatter.getKeyName(keys[0]);
+            fuck[1][i] = InputFormatter.getKeyName(keys[1]);
+        }
+        for(key in fuck[0]) shit += key + " ";
+        shit+= "/ ";
+        for(key in fuck[1]) shit += key + " ";
+
+        var leReset:Array<Dynamic> = ClientPrefs.keyBinds.get('reset');
+        var leAccept:Array<Dynamic> = ClientPrefs.keyBinds.get('accept');
         var tipTextArray:Array<String> =
-        "Comandi Movimento - Muoviti
-        \nShift - Movimento piu' veloce
-        \nE/Q - Camera Zoom Avanti/Indietro
-		\nR - Reset Camera Zoom
-		\nAccetta per continuare".split('\n');
+        '$shit- Muoviti
+        \nShift - Movimento piu\' veloce
+        \nE / Q - Camera Zoom Avanti/Indietro
+		\n${InputFormatter.getKeyName(leReset[0])} / ${InputFormatter.getKeyName(leReset[1])} - Reset Camera Zoom
+		\n${InputFormatter.getKeyName(leAccept[0])} / ${InputFormatter.getKeyName(leAccept[1])} per continuare'.split('\n');
 
 		for (i in 0...tipTextArray.length-1)
 		{
-			var tipText:FlxText = new FlxText(FlxG.width - 320, FlxG.height - 15 - 16 * (tipTextArray.length - i), 300, tipTextArray[i], 12);
+			var tipText:FlxText = new FlxText(FlxG.width - 1020, FlxG.height - 15 - 16 * (tipTextArray.length - i), 1000, tipTextArray[i], 12);
 			tipText.cameras = [camHUD];
 			tipText.setFormat(null, 12, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE_FAST, FlxColor.BLACK);
 			tipText.scrollFactor.set();
@@ -64,62 +81,78 @@ class JenpactCutScene extends MusicBeatState
         pulsantone.animation.play('idle');
         add(pulsantone);
 
+        FlxG.sound.playMusic(Paths.music('bgmjen'), 0);
+        FlxG.sound.music.pitch = 0;
+        FlxG.sound.music.volume = 1;
+	    lol = FlxTween.tween(FlxG.sound.music, {pitch: 1}, 2);
+
         super.create();
         CustomFadeTransition.nextCamera = camOther;
     }
 
+    private var selectedSmth:Bool = false;
     override function update(elapsed:Float)
     {
-        if (FlxG.keys.justPressed.R) {
-            FlxG.camera.zoom = 1;
-        }
-
-        if (FlxG.keys.pressed.E && FlxG.camera.zoom < 3) {
-            FlxG.camera.zoom += elapsed * FlxG.camera.zoom;
-            if(FlxG.camera.zoom > 3) FlxG.camera.zoom = 3;
-        }
-        if (FlxG.keys.pressed.Q && FlxG.camera.zoom > 0.1) {
-            FlxG.camera.zoom -= elapsed * FlxG.camera.zoom;
-            if(FlxG.camera.zoom < 0.1) FlxG.camera.zoom = 0.1;
-        }
-
-        if (controls.UI_UP || controls.UI_DOWN || controls.UI_LEFT || controls.UI_RIGHT)
+        if (!selectedSmth)
         {
-            var addToCam:Float = 500 * elapsed;
-            if (FlxG.keys.pressed.SHIFT)
-                addToCam *= 4;
+            if (FlxG.keys.justPressed.R) {
+                FlxG.camera.zoom = 1;
+            }
 
-            if (controls.UI_UP && camFollow.y > -360)
-                camFollow.y -= addToCam;
-            else if (controls.UI_DOWN && camFollow.y < 720)
-                camFollow.y += addToCam;
+            if (FlxG.keys.pressed.E && FlxG.camera.zoom < 3) {
+                FlxG.camera.zoom += elapsed * FlxG.camera.zoom;
+                if(FlxG.camera.zoom > 3) FlxG.camera.zoom = 3;
+            }
+            if (FlxG.keys.pressed.Q && FlxG.camera.zoom > 0.1) {
+                FlxG.camera.zoom -= elapsed * FlxG.camera.zoom;
+                if(FlxG.camera.zoom < 0.1) FlxG.camera.zoom = 0.1;
+            }
 
-            if (controls.UI_LEFT && camFollow.x > -640)
-                camFollow.x -= addToCam;
-            else if (controls.UI_RIGHT && camFollow.x < 1280)
-                camFollow.x += addToCam;
-        }
+            if (controls.UI_UP || controls.UI_DOWN || controls.UI_LEFT || controls.UI_RIGHT)
+            {
+                var addToCam:Float = 500 * elapsed;
+                if (FlxG.keys.pressed.SHIFT)
+                    addToCam *= 4;
 
-        if (controls.ACCEPT) {
-            FlxG.sound.play(Paths.sound('confirmMenu'));
-            pulsantone.animation.play('click');
-            new FlxTimer().start(0.4, function(tmr:FlxTimer) if (PlayState.SONG.player1 == null) {
-                LoadingState.loadAndSwitchState(new CharSelectorState());
-                FreeplayState.skinSelected = true;
-            } else {
-                LoadingState.loadAndSwitchState(new PlayState());
-                FlxG.sound.music.volume = 0;
-            });
-        }
-        else if (controls.BACK) {  // Resetto anche le variabili del Playstate per sicurezza  - Nex
-            FlxG.sound.play(Paths.sound('cancelMenu'));
-            PlayState.deathCounter = 0;
-            PlayState.seenCutscene = false;
-            WeekData.loadTheFirstEnabledMod();
-            MusicBeatState.switchState(new FreeplayState());
-            FlxG.sound.playMusic(Paths.music('freakyMenu'));
-            PlayState.changedDifficulty = false;
-            PlayState.chartingMode = false;
+                if (controls.UI_UP && camFollow.y > -360)
+                    camFollow.y -= addToCam;
+                if (controls.UI_DOWN && camFollow.y < 1080)
+                    camFollow.y += addToCam;
+                if (controls.UI_LEFT && camFollow.x > -640)
+                    camFollow.x -= addToCam;
+                if (controls.UI_RIGHT && camFollow.x < 1920)
+                    camFollow.x += addToCam;
+            }
+
+            if (controls.ACCEPT) {
+                FlxG.sound.play(Paths.sound('confirmMenu'));
+                pulsantone.animation.play('click');
+                selectedSmth = true;
+
+                new FlxTimer().start(0.4, function(tmr:FlxTimer) {
+                    if (!lol.finished) lol.cancelChain();
+                    if (PlayState.SONG.player1 == null) {
+                        LoadingState.loadAndSwitchState(new CharSelectorState());
+                        FreeplayState.skinSelected = true;
+                        FlxTween.tween(FlxG.sound.music, {pitch: 0}, 0.6);
+                    } else {
+                        LoadingState.loadAndSwitchState(new PlayState());
+                        FlxG.sound.music.volume = 0;
+                    }
+                });
+            }
+            else if (controls.BACK) {  // Resetto anche le variabili del Playstate per sicurezza  - Nex
+                selectedSmth = true;
+                if (!lol.finished) lol.cancelChain();
+                FlxG.sound.play(Paths.sound('cancelMenu'));
+                PlayState.deathCounter = 0;
+                PlayState.seenCutscene = false;
+                WeekData.loadTheFirstEnabledMod();
+                MusicBeatState.switchState(new FreeplayState());
+                FlxG.sound.playMusic(Paths.music('freakyMenu'));
+                PlayState.changedDifficulty = false;
+                PlayState.chartingMode = false;
+            }
         }
 
         super.update(elapsed);
--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -105,9 +105,9 @@ class MainMenuState extends MusicBeatState
 		for (i in 0...optionShit.length)
 		{
 			var offset:Float = 108 - (Math.max(optionShit.length, 4) - 4) * 80;
-			var menuItem:FlxSprite = new FlxSprite(0, (i * 160)  + offset);
-			menuItem.scale.x = optionShit[i] == 'shop_locked' ? 0.85 : 1;
-			menuItem.scale.y = optionShit[i] == 'shop_locked' ? 0.85 : 1;
+			var menuItem:FlxSprite = new FlxSprite(0, (i * (optionShit[i] == 'donate' ? 155 : 160)) + offset);
+			menuItem.scale.x = optionShit[i] == 'shop_locked' ? 0.85 : optionShit[i] == 'donate' ? 0.65 : 1;
+			menuItem.scale.y = optionShit[i] == 'shop_locked' ? 0.85 : optionShit[i] == 'donate' ? 0.65 : 1;
 			menuItem.frames = Paths.getSparrowAtlas('mainmenu/menu_' + optionShit[i]);
 			menuItem.animation.addByPrefix('idle', optionShit[i] + " basic", 24);
 			menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
