2b23e8fbce5dabc9af2cb7db3e0e4002e29e54d1
⍚~Nex <87421482+NexIsDumb@users.noreply.github.com>
2023-08-21 22:54:58 +0200

    CRISTO

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -122,7 +122,7 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollowPos, null, 1);
 
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 64, 0, "Poldhub DEMO", 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 64, 0, "Poldhub DEMO v1.1", 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
